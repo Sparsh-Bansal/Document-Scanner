22b5a00382e08bfb175e5087272c13d506d01a08
Sparsh-Bansal <44178670+Sparsh-Bansal@users.noreply.github.com>
2019-07-07 23:03:15 +0530

    updated

--- scanner.py
+++ scanner.py
@@ -73,13 +73,14 @@ import numpy as np
 import imutils
 import argparse
 from skimage.filters import threshold_local
+import math
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-i' , '--image' , required=True)
 args = vars(ap.parse_args())
 
 def thresholding(img):
-    dilated_img = cv2.dilate(img, np.ones((7, 7), np.uint8))
+    dilated_img = cv2.dilate(img, np.ones((5,5), np.uint8))
     bg_img = cv2.medianBlur(dilated_img, 21)
     diff_img = 255 - cv2.absdiff(img, bg_img)
     norm_img = diff_img.copy()  # Needed for 3.x compatibility
@@ -88,6 +89,12 @@ def thresholding(img):
     cv2.normalize(thr_img, thr_img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
     return thr_img
 
+def angle(x1,y1,x2,y2,x3,y3):
+	m1 = (y2-y1)/(x2-x1)
+	m2 = (y3-y2)/(x3-x2)
+	m = np.absolute((m1-m2)/(1+(m1*m2)))
+	ang = math.atan(m)
+	return ang
 
 def document_scanner(original_image,heights):
     global original
@@ -98,8 +105,9 @@ def document_scanner(original_image,heights):
         original_image = imutils.resize(original_image, height=height)
         gray = cv2.cvtColor(original_image , cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray,(5,5),0)
-        _, gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        # gray = thresholding(original_image)
+        # gray = cv2.adaptiveThreshold(gray , 255 , cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,10,0)
+        # _, gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        gray = thresholding(original_image)
         cv2.imshow('Gray2',gray)
 
         print('Height',height)
@@ -129,27 +137,43 @@ def document_scanner(original_image,heights):
                     screenCnt = approx
                     check = True
                     break
-            if check:
-                print("STEP 2: Find contours of paper")
-                cv2.drawContours(original_image, [screenCnt], -1, (0, 255, 0), 2)
-                cv2.imshow("Outline", original_image)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-                break
-            else:
-                continue
+            print(screenCnt)
+            print('Sreencnt',screenCnt[0][0][0])
+            print(screenCnt[1][0])
+            print(screenCnt[1][0][0])
+            a1 = angle(screenCnt[0][0][0],screenCnt[0][0][1],screenCnt[1][0][0],screenCnt[1][0][1],screenCnt[2][0][0],screenCnt[2][0][1])
+            a2 = angle(screenCnt[1][0][0],screenCnt[1][0][1],screenCnt[2][0][0],screenCnt[2][0][1],screenCnt[3][0][0],screenCnt[3][0][1])
+            a3 = angle(screenCnt[2][0][0],screenCnt[2][0][1],screenCnt[3][0][0],screenCnt[3][0][1],screenCnt[0][0][0],screenCnt[0][0][1])
+            a4 = angle(screenCnt[3][0][0],screenCnt[3][0][1],screenCnt[0][0][0],screenCnt[0][0][1],screenCnt[1][0][0],screenCnt[1][0][1])
+
+            if a1<1.744 and a1>1.395 and a2<1.744 and a2>1.395 and a3<1.744 and a3>1.395 and a4<1.744 and a4>1.395:
+                if check:
+                    print("STEP 2: Find contours of paper")
+                    cv2.drawContours(original_image, [screenCnt], -1, (0, 255, 0), 2)
+                    cv2.imshow("Outline", original_image)
+                    cv2.waitKey(0)
+                    cv2.destroyAllWindows()
+                    break
+                else:
+                    continue
         if check:
             break
         else:
             continue
 
+    if a1<1.744 and a1>1.395 and a2<1.744 and a2>1.395 and a3<1.744 and a3>1.395 and a4<1.744 and a4>1.395:
 
-    warped = four_point_transform(original, screenCnt.reshape(4, 2) *ratio)
-    warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-    # _,warped = cv2.threshold(warped , 0 , 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    cv2.imshow('YOY',warped)
-    T = threshold_local(warped, 11, offset=10, method="gaussian")
-    warped = (warped > T).astype("uint8") * 255
+        warped = four_point_transform(original, screenCnt.reshape(4, 2) *ratio)
+        warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+        # _,warped = cv2.threshold(warped , 0 , 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        cv2.imshow('YOY',warped)
+        T = threshold_local(warped, 11, offset=10, method="gaussian")
+        warped = (warped > T).astype("uint8") * 255
+    else:
+        warped = original
+        warped = cv2.cvtColor(warped , cv2.COLOR_BGR2GRAY)
+        T = threshold_local(warped ,15,offset=4,method='gaussian')
+        warped = (warped>T).astype('uint8')*255
     return warped
 
 img = cv2.imread(args['image'])
@@ -166,10 +190,17 @@ try:
 except:
     print("Image Not Captured Properly")
     warped = img
+    warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+    T = threshold_local(warped, 15, offset=4, method='gaussian')
+    warped = (warped > T).astype('uint8') * 255
 
 
 
 print("STEP 3: Apply perspective transform")
 cv2.imshow("Original", imutils.resize(original, height=650))
 cv2.imshow("Scanned", imutils.resize(warped, height=650))
+filename = args['image'].split('.')[0]+'_detected.jpg'
+filename = 'output/'+filename
+# cv2.imwrite(filename , warped)
+print(filename)
 cv2.waitKey(0)
